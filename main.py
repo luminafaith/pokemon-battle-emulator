d33518a9c96c2fcc2289fdd0bfb16be1c0d61c23
luminafaith <leah.young7061@gmail.com>
2023-09-26 21:02:07 -0400

    Add basic damage calculation function

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Pokemon:
     def __init__(self, pokeNameInputted, pokeTypeInputted):
         self.pokeName = pokeNameInputted
@@ -42,4 +45,28 @@ def calcDamage(attacker, defender, move):
     calcPB = 1
     calcWeather = 1 # TODO: add weather
     calcGlaiveRush = 1 # TODO: add glaive rush move (gen 9, a long ways off)
-    return 5
+    calcCritical = 1 # TODO: add critical hit mechanics; rounds down to the nearest integer
+    calcRandom = random.randint(85, 100) / 100 # TODO: rounds down to the nearest integer
+    if(move.moveType == attacker.pokeType):
+        calcSTAB = 1.5
+    else:
+        calcSTAB = 1
+    calcTypeEff = 1 # TODO: add type effectiveness mechanics; rounds down to nearest intger
+    calcBurn = 1 # TODO: add status effect check for burn
+    calcOther = 1 # TODO: add held items and abilities
+    calcZMove = 1 # TODO: add z-moves
+
+    # doing the calculation
+    step1 = (2 * calcLevel / 5) + 2
+    step2 = step1 * calcPower * (calcA / calcD)
+    step3 = (step2 / 50) +2
+    step4 = step3 * calcTarget * calcPB * calcWeather * calcGlaiveRush * calcCritical
+    step5 = round(step4)
+    step6 = step5 * calcRandom
+    step7 = round(step6)
+    step8 = step7 * calcSTAB * calcTypeEff
+    step9 = round(step8)
+    step10 = step9 * calcBurn * calcOther * calcZMove
+    step11 = round(step10)
+    
+    return step11
